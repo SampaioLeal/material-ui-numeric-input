3972ce349659ee2146fcdae3559b888f7c62d55b
Sampaio Leal <samp14hd@gmail.com>
2020-09-30 15:22:57 -0300

    Updates

---
+++ precache-manifest.75e4b3afc7385c2936ddb1a0ae90e72f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ecf0d34283509b4926f04f98aab9e01",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9af767d1de885fe5f039",
+    "url": "./static/css/main.7a14ee14.chunk.css"
+  },
+  {
+    "revision": "2255ee727a8e0d91f11f",
+    "url": "./static/js/2.1c2c0800.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "./static/js/2.1c2c0800.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9af767d1de885fe5f039",
+    "url": "./static/js/main.1e77fe95.chunk.js"
+  },
+  {
+    "revision": "622d1866e5ffd8353e14",
+    "url": "./static/js/runtime-main.6d7fab11.js"
+  }
+]);
